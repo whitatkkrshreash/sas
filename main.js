8afbd1e0f60c8f1853f86917a32446d0996ae086
whitatkkrshreash <79008006+whitatkkrshreash@users.noreply.github.com>
2021-05-16 17:55:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function addUser()
+{
+player1_name = document.getElementById("player1_name_input").value;
+player2_name = document.getElementById("player2_name_input").value;
+
+localStorage.setItem("player1_name", player1_name);
+localStorage.setItem("player2_name", player2_name);
+
+window.location = "index.html";
+document.getElementById(result_1).innerHTML = localStorage.getItem("player1_name");
+document.getElementById(result_2).innerHTML = localStorage.getItem("player2_name");
+}
+
+function send()
+{
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) *parseInt(number2);
+}
